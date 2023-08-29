6e39cd86c1f88ed78b52cc6c28f88a4f5ac43b74
Ziv_Y_Chang <ziv_y_chang@trendmicro.com>
2023-08-29 21:36:32 +0800

    rewording

--- budget_test.py
+++ budget_test.py
@@ -31,17 +31,17 @@ class BudgetTestCase(unittest.TestCase):
         budget = self.budget_svc.query(start = datetime(2023,9,1), end = datetime(2023,9,30))
         self.assertEqual(budget, 30.0)
 
-    def test_get_budget_across_month(self):
+    def test_get_budget_across_months(self):
         budget = self.budget_svc.query(start = datetime(2023,7,1), end = datetime(2023,8,7))
         self.assertEqual(budget, 1010.0)
         budget = self.budget_svc.query(start = datetime(2023,7,30), end = datetime(2023,9,2))
         self.assertEqual(budget, 3122.0)
 
-    def test_get_illegal_date(self):
+    def test_illegal_date(self):
         budget = self.budget_svc.query(start = datetime(2023,8,7), end = datetime(2023,7,8))
         self.assertEqual(budget, 0)
 
-    def test_get_no_data(self):
+    def test_no_data(self):
         budget = self.budget_svc.query(start = datetime(2099,3,1), end = datetime(2099,3,2))
         self.assertEqual(budget, 0)
 
